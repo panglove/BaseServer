158b3204c8ed99fd740d8b64fc774eb190b5ffa3
seelove792 <1294614342@qq.com>
2021-01-23 01:27:18 +0800

    add file bytes

--- util/crypto/des.go
+++ util/crypto/des.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/marspere/goencrypt"
 )
-const PassWord  = "mZKM454z"
+var PassWord  = "mZKM454z"
 func DesEncode(str string) (string, error) {
 	// key为12345678
 	// iv为空
